14e03796efbb0de17a594cd4153eee88cedca662
Monjur Hasan <51787931+monjurhasan4404@users.noreply.github.com>
2022-12-16 20:27:34 +0600

    make clock

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import ReactDOM from 'react-dom';
+class Clock extends React.Component {
+  render(){
+    return(
+     <h1 className="heading">
+       <span className="text">Hello {new Date().toLocaleTimeString(this.prop.locale)}
+        </span>
+      </h1>  
+     );
+     
+}
+}
+ReactDOM.render( <Clock locale="bn-BD"/>, document.getElemetById('root'));
